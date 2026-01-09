fdfcfc4ec6880da7ad394636f1f80e0de03b7f75
Raksa <hoeunraksa012@gmail.com>
2026-01-09 22:23:28 +0700

    ....

--- config/cors.php
+++ config/cors.php
@@ -43,7 +43,10 @@ return [
     'supports_credentials' => false,
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
     'allowed_headers' => ['*'],
     'supports_credentials' => true,
 ];
