5b39d87482e1fa911c953d91e73b511489a33d1a
Raksa <hoeunraksa012@gmail.com>
2026-01-11 00:55:07 +0700

    ....

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://study.learner-teach.online'
+        'https://study.learner-teach.online',
+        'https://smart-registration-frontend.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
