27ce8c8dcc42c12aa05eb8cb3776c1da37c92264
Raksa <hoeunraksa012@gmail.com>
2026-01-14 03:58:21 +0700

    ...

--- routes/api.php
+++ routes/api.php
@@ -52,12 +52,15 @@ Route::get('/subjects/{id}', [SubjectController::class, 'show']);
 | PAYMENT (PUBLIC + AUTHENTICATED)
 |--------------------------------------------------------------------------
 */
-Route::prefix('payment')->group(function () {
-    Route::post('/generate-qr', [PaymentController::class, 'generateQr']);
-    Route::get('/check-status/{tranId}', [PaymentController::class, 'checkPaymentStatus']);
-    Route::post('/callback', [PaymentController::class, 'paymentCallback']); // MUST stay public
-    Route::get('/registration/{registrationId}', [PaymentController::class, 'getRegistrationPayment']);
-});
+Route::prefix('payment')
+    ->middleware([]) // 🔥 FORCE NO MIDDLEWARE
+    ->group(function () {
+        Route::post('/generate-qr', [PaymentController::class, 'generateQr']);
+        Route::get('/check-status/{tranId}', [PaymentController::class, 'checkPaymentStatus']);
+        Route::post('/callback', [PaymentController::class, 'paymentCallback']);
+        Route::get('/registration/{registrationId}', [PaymentController::class, 'getRegistrationPayment']);
+    });
+
 
 
 /*
