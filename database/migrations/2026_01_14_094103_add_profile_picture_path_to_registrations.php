163511f487a8732ce0f98cec4b653f559a510744
Raksa <hoeunraksa012@gmail.com>
2026-01-14 16:59:50 +0700

    ....

--- database/migrations/2026_01_14_094103_add_profile_picture_path_to_registrations.php
+++ database/migrations/2026_01_14_094103_add_profile_picture_path_to_registrations.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::table('registrations', function (Blueprint $table) {
             $table->string('profile_picture_path', 255)
                   ->nullable()
-                  ->change(); // 👈 MODIFY, not ADD
+                  ->change();
         });
     }
 
