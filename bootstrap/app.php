6c7e43c78268dc4ee3351f63d2c1938734d6e104
Raksa <hoeunraksa012@gmail.com>
2026-01-14 19:00:53 +0700

    ....

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -34,7 +34,5 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => RoleMiddleware::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
-    })
     ->create();
+
