fdf1bd64b0297c89864f3afd9a7592685db87a8c
vathana <siengvathana1@gmail.com>
2026-01-24 00:49:44 +0700

    update

---
+++ database/migrations/2026_01_23_174744__recreate_message_again.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::dropIfExists('messages');
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('sender_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->foreignId('receiver_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->text('content')->nullable(); // text message
+
+            $table->boolean('is_read')->default(false);
+
+            $table->timestamps();
+
+            $table->index(['sender_id', 'receiver_id']);
+        });
+        
+        Schema::create('message_attachments', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('message_id')
+                ->constrained('messages')
+                ->onDelete('cascade');
+
+            $table->enum('type', ['image', 'file', 'audio']);
+            $table->string('file_path');
+            $table->string('original_name')->nullable();
+            $table->integer('file_size')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
