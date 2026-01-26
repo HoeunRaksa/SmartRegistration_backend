4f32b76e01b56449487c984333d262bd1be17550
Raksa <hoeunraksa012@gmail.com>
2026-01-26 22:46:29 +0700

    ...

--- app/Http/Controllers/Api/StudentMessageController.php
+++ app/Http/Controllers/Api/StudentMessageController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Conversation;
+use App\Models\MessageAttachment;;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
